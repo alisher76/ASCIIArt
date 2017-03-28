beac68351f912b59355bd37acea6a824a7548e6d
Ali•Gorithm <alisher.coder@gmail.com>
2017-03-28 10:26:09 -0400

    Initial Commit

---
+++ AAli.AssignmentASCIIArt.swift
@@ -0,0 +1,191 @@
+import Foundation
+import Darwin
+
+func clearScreen() {
+    Process.launchedProcess(launchPath: "/usr/bin/clear", arguments: []).waitUntilExit()
+}
+
+clearScreen()
+print("Hello")
+
+sleep(2)
+clearScreen()
+
+print("Wanna see a new car?")
+
+sleep(2)
+clearScreen()
+
+
+print("Here is a new Tesla's car:")
+sleep(2)
+clearScreen()
+    
+    
+print(" ####   ####  #        ##   #####  ")
+print("#      #    # #       #  #  #    # ")
+print(" ####  #    # #      #    # #    # ")
+print("     # #    # #      ###### #####  ")
+print("#    # #    # #      #    # #   #  ")
+print(" ####   ####  ###### #    # #    # ")
+sleep(2)
+clearScreen()
+    
+print("   ")
+print("   ")
+print("   ")
+print("   ")
+print("   ")
+sleep(1)
+clearScreen()
+
+print("    ")
+print("    ")
+print("`-. ")
+print(")-' ")
+print("    ")
+sleep(1)
+clearScreen()
+
+print("       ")
+print("__     ")
+print(",-.`-. ")
+print(" o )-' ")
+print("`-'    ")
+sleep(1)
+clearScreen()
+    
+    
+print(" _____")
+print(" _____\\__")
+print("  -|-  ,-.`-.")
+print(" )----( o )-'")
+print("       `-'")
+sleep(1)
+clearScreen()
+
+    
+print("    ________")
+print("-- /\\_____\\__")
+print("_ / ,-. -|-  ,-.`-.")
+print(" _`( o )----( o )-'")
+print("    `-'      `-'")
+sleep(1)
+clearScreen()
+
+    
+    
+    
+print("            ________")
+print("    _-_-  _/\\______\\__")
+print(" _-_-__  / ,-. -|-  ,-.`-.")
+print("    _-_- `( o )----( o )-'")
+print("           `-'      `-'")
+print()
+sleep(1)
+clearScreen()
+
+print("              ________            ")
+print("      _-_-  _/\\______\\__        ")
+print("   _-_-__  / ,-. -|-  ,-. `-.     ")
+print("      _-_- `( o )----( o )-'      ")
+print("             `-'      `-'         ")
+print()
+sleep(1)
+clearScreen()
+
+
+
+
+print("                   ________            ")
+print("       _ - _ -   _/\\______\\__        ")
+print("   _- _- _ _    / ,-. -|-  ,-. `-.     ")
+print("       _ - _ -  `( o )----( o )-'      ")
+print("                  `-'      `-'         ")
+print()
+sleep(1)
+clearScreen()
+
+print("_ _                        ________            ")
+print("_` |           _ - _ -   _/\\______\\__        ")
+print("_| |       _- _- _ _    / ,-. -|-  ,-. `-.     ")
+print("_| |           _ - _ -  `( o )----( o )-'      ")
+print("                          `-'      `-'         ")
+print()
+sleep(1)
+clearScreen()
+
+
+
+print("  ___| | __ _                    ________            ")
+print(" / __| |/ _` |       _ - _ -   _/\\______\\__        ")
+print(" |__ | | (_| |    - _- _ _    / ,-. -|-  ,-. `-.     ")
+print(" __/_| __,_| |       _ - _ -  `( o )----( o )-'      ")
+print("                                `-'      `-'         ")
+print()
+sleep(1)
+clearScreen()
+
+
+print("| |_ ___  ___| | __ _                 ________            ")
+print("| __/ _ |/ __| |/ _` |    _ - _ -   _/\\______\\__        ")
+print("| ||  __/|__ | | (_| | - _- _ _    / ,-. -|-  ,-. `-.     ")
+print("|_____||___/_| __,_| |    _ - _ -  `( o )----( o )-'      ")
+print("                                     `-'      `-'         ")
+print()
+sleep(1)
+clearScreen()
+
+print("    | |_ ___  ___| | __ _                    ________        ")
+print("    | __/ _ |/ __| |/ _` |    _  -  _ -    _/\\______\\__    ")
+print("    | ||  __/|__ | | (_| | - _-  _  _     / ,-. -|-  ,-. `-. ")
+print("    |_____||___/_| __,_| |    _  -  _ -   `( o )----( o )-'  ")
+print("                                            `-'      `-'     ")
+print()
+sleep(1)
+clearScreen()
+    
+    
+print("              | |_ ___  ___| | __ _                    _____ ")
+print("    _  -  _ - | __/ _ |/ __| |/ _` |    _  -  _ -    _/\\___ ")
+print(" - _-  _  _   | ||  __/|__ | | (_| | - _-  _  _     / ,-. -| ")
+print("    _  -  _ - |_____||___/_| __,_| |    _  -  _ -   `( o )-- ")
+print("                                                      `-'    ")
+print()
+sleep(1)
+clearScreen()
+
+
+print("                  | |_ ___  ___| | __ _                   ")
+print("        _  -  _ - | __/ _ |/ __| |/ _` |    _  -  _ -     ")
+print("     - _-  _  _   | ||  __/|__ | | (_| | - _-  _  _       ")
+print("        _  -  _ - |_____||___/_| __,_| |    _  -  _ -     ")
+print("                                                          ")
+print()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    
